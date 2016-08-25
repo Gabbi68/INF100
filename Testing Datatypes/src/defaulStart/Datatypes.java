284d69a71209e84e214f1701badf556eb47979d2
Dj_Usher <Dj_Usher@84.211.55.36>
2016-08-25 02:31:48 +0200

    Adding another project

---
+++ Testing Datatypes/src/defaulStart/Datatypes.java
@@ -0,0 +1,24 @@
+package defaulStart;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		
+		String Bokstaver = "Hello darkness my old friend!"; //C# String, eclipse uthever ikke :(
+		long StortHeltall = 20000; //C# long
+		int Heltall = 42; //C# int(32) uten mulighet for 12/64
+		double Decimal = 42.42; //C# decimal
+		boolean Biner = true; //C# sin bool, uten mulighet for definisjon utenfor true/false (0/1)
+		float Flyttall = 1.0F; //C# float
+		
+		System.out.println(Bokstaver);
+		System.out.println(StortHeltall);
+		System.out.println(Heltall);
+		System.out.println(Decimal);
+		System.out.println(Biner);
+		System.out.println(Flyttall);
+		
+
+	}
+
+}
