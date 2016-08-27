e0cce9555f6bee982de9b133f9644bf00d172601
RakNoel <oskar.leir@gmail.com>
2016-08-27 18:20:40 +0200

    started task 1 an 2 chapter 2
    also started testing some loopa and modulo "%"

--- Oppgaver/src/Kapittel2/P2_02.java
+++ Oppgaver/src/Kapittel2/P2_02.java
@@ -1,5 +1,5 @@
 package Kapittel2;
 
 public class P2_02 {
-  //Has not yet started on this
+
 }
---
+++ Oving-1/src/oppgaver/loopModuloTest.java
@@ -0,0 +1,14 @@
+package oppgaver;
+
+import javax.swing.*;
+
+/**
+ * Created by Grønevikveien 4 on 27.08.2016.
+ */
+public class loopModuloTest {
+    public static void main(String[] args){
+        try{
+            double input1 = Double.parseDouble(JOptionPane.showInputDialog("How long would you like it to be?"));
+        }
+    }
+}
