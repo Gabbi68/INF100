7ceafa8e8aaf216b92edad08464a601142c532ad
RakNoel <oskar.leir@gmail.com>
2016-08-29 21:46:43 +0200

    started testing some loops and modulo "%"

--- Oving-1/src/oppgaver/loopModuloTest.java
+++ Oving-1/src/oppgaver/loopModuloTest.java
@@ -1,14 +1,21 @@
 package oppgaver;
 
 import javax.swing.*;
+import java.io.IOException;
 
 /**
  * Created by Grønevikveien 4 on 27.08.2016.
  */
 public class loopModuloTest {
-    public static void main(String[] args){
-        try{
-            double input1 = Double.parseDouble(JOptionPane.showInputDialog("How long would you like it to be?"));
+    public static void main(String[] args) {
+        double input1 = Double.parseDouble(JOptionPane.showInputDialog("How long would you like it to be?"));
+
+        for (int i = 0; i < input1; i++) {
+                if(i%2 == 0){
+                    System.out.println("|  |");
+                }else{
+                    System.out.println("++++");
+                }
         }
     }
 }
